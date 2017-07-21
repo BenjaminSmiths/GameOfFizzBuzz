2b7f2224cfd46c2bff68cacd6e1a885955f78324
Ben Smith <benjamin.smith@sky.uk>
2017-07-21 22:53:46 +0100

    Fixed some any[] array types, Added some doc notes to the public methods and readme file.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GameOfFizzBuzz
-Code Test
+
+Here is the game of fizz buzz a classic one for you to play.
+You might even get lucky once in awhile ;)
+
+To run this code example:
+
+    npm i
+    npm test
+    npm start
--- app/FizzBuzz.ts
+++ app/FizzBuzz.ts
@@ -9,36 +9,28 @@ export type WordType = 'fizz'|'buzz'|'fizzbuzz'|'lucky'
 
 export class FizzBuzz {
 
-    private _reports: any[] = [];
+    private _reports: (WordType|number)[] = [];
 
-    constructor(readonly sequence: any[]) {
+    constructor(readonly sequence: number[]) {
 
     }
 
+    /**
+     * Build a report string with the number of alterations for each word changed
+     * and a report for the unchanged integers remaining.
+     *
+     * @returns {string}
+     */
     report(): string {
-        return this._reports ? [...this._wordReport.concat(), ...this._numberReport()].join(' ') : '';
+        return this._reports ? [...this._wordReports, ...this._numberReports].join(' ') : '';
     }
 
-    private get _wordReport(): any[] {
-        return Object.keys(WORDS).map((key: string) => {
-            let word: string = (WORDS as any)[key];
-            return `${word}: ${this._count(word)}`
-        });
-    }
-
-    private _numberReport(): any[] {
-        return [`integer: ${this._reports.filter(n => isNaN(n as number)).length}`];
-    }
-
-    private _count(type: string): number {
-        let filtered = this._reports.filter(n => {
-            return n === type
-        });
-        return filtered ? filtered.length : 0;
-    }
-
-
-    play(): any[] {
+    /**
+     * Will run though the sequence of numbers and replace the fizz, buzz, fizzbuzz and licky numbers with words
+     *
+     * @returns {[(WordType|any)]}
+     */
+    play(): (WordType|number)[] {
         return this._reports = this.sequence.map(number => {
             switch (true) {
                 case String(number).includes('3'):
@@ -54,4 +46,27 @@ export class FizzBuzz {
             }
         })
     }
+
+    //////////////////////////////////////////////////////////
+    // Private Methods                                      //
+    //////////////////////////////////////////////////////////
+
+    private get _wordReports(): string[] {
+        return Object.keys(WORDS).map((key: string) => {
+            let word: string = (WORDS as any)[key];
+            return `${word}: ${this._count(word)}`
+        }).concat();
+    }
+
+    private get _numberReports(): string[] {
+        return [`integer: ${this._reports.filter(n => isNaN(n as number)).length}`];
+    }
+
+    private _count(type: string): number {
+        let filtered = this._reports.filter(n => {
+            return n === type
+        });
+        return filtered ? filtered.length : 0;
+    }
+
 }
